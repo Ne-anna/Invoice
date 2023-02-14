f40f3f371fef7037441592adfc1d053dd568ffd6
Ne-anna <annabusele@inbox.lv>
2023-02-14 09:14:24 +0200

    Created "ItemDescription" class and list of items in program.cs file.

---
+++ Invoice/ItemDescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice
+{
+    public class ItemDescription
+    {
+        public string partNumber;
+        public string partDescription;
+        public int quantity;
+        public int price;
+
+        public ItemDescription(string partNumber, string partDescription, int quantity, int price)
+        {
+            PartNumber = partNumber;
+            PartDescription = partDescription;
+            Quantity = (quantity >= 0) ? quantity : 0;
+            Price = (price >= 0) ? price : 0;
+        }
+
+        public string PartNumber { get; set; }
+        public string PartDescription { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
--- Invoice/Program.cs
+++ Invoice/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+
+            ItemDescription[] itemDescriptions = new ItemDescription[11];
+            itemDescriptions[0] = new ItemDescription("00", "PhotographyItem", 4, 17);
+            itemDescriptions[1] = new ItemDescription("01", "WakeboardItem", 2, 10);
+            itemDescriptions[2] = new ItemDescription("02", "DrumSetItem", 7, 170);
+            itemDescriptions[3] = new ItemDescription("03", "PaintingItem", 8, 100);
+            itemDescriptions[4] = new ItemDescription("04", "PhotographyItem", 20, 12);
+            itemDescriptions[5] = new ItemDescription("05", "WakeboardItem", 17, 14);
+            itemDescriptions[6] = new ItemDescription("06", "DrumSetItem", 24, 27);
+            itemDescriptions[7] = new ItemDescription("07", "PaintingItem", 27, 49);
+            itemDescriptions[8] = new ItemDescription("08", "LongboardItem", 11, 50);
+            itemDescriptions[9] = new ItemDescription("09", "SwimmingItem", 40, 2);
+            itemDescriptions[10] = new ItemDescription("010", "PetItem", 47, 33);
+
+            foreach (var item in itemDescriptions)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
